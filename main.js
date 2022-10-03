61665d300ded00bac550636d00a7dcd43d2aa3d7
Kota Yatagai <kota@yatagai.com>
2022-10-03 05:09:37 +0000

    Modify sitekey

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const onloadTurnstileCallback = () => {
   const opt = {
-    sitekey: "0x4AAAAAAAAsaSc3yvVEoUpH",
+    sitekey: "0x4AAAAAAAAsao7t-AWgR25T",
     theme: "light",
     callback: (token) => {
       console.log(`Challenge Success ${token}`);
